288828422d313e5303c608a5443f1ea20eaa8fdd
Hugo Damitzian <hdamitzi@z4r5p1.42lyon.fr>
2022-11-14 16:05:47 +0100

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,5 +14,17 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	
+	char	*str1;
+	char	*str2;
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+		i++;
+	}
+	return (0);
 }
