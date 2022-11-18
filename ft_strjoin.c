484ec2eab6ce869d0952255b7f927354d9243c94
Hugo Damitzian <hdamitzi@z3r9p1.42lyon.fr>
2022-11-18 10:41:30 +0100

    header + correction

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,11 +1,22 @@
-#include "libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdamitzi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 10:40:06 by hdamitzi          #+#    #+#             */
+/*   Updated: 2022/11/18 10:40:14 by hdamitzi         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*new;
 	int		j;
-	
+
 	if (!s1 || !s2)
 		return (NULL);
 	new = (char *)malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
