9105966c5fd7ff2ecc0c8a80a6e48b58d671d6aa
Hugo Damitzian <hdamitzi@z2r7p2.42lyon.fr>
2022-11-25 12:21:19 +0100

    second try first commit

---
+++ ft_lstmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdamitzi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 12:23:14 by hdamitzi          #+#    #+#             */
+/*   Updated: 2022/11/21 12:23:17 by hdamitzi         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include  "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*newlist;
+	t_list	*newelement;
+
+	newlist = NULL;
+	while (lst)
+	{
+		newelement = ft_lstnew(f(lst->content));
+		if (!newelement)
+		{
+			ft_lstclear(&newlist, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&newlist, newelement);
+		lst = lst->next;
+	}
+	return (newlist);
+}
