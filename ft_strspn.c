7484ed3b979004b97ab2cac07d97dfb9bfe89ba4
Hugo Damitzian <hdamitzi@student.42lyon.fr>
2023-03-07 16:43:41 +0100

    norminette

--- ft_strspn.c
+++ ft_strspn.c
@@ -6,7 +6,7 @@
 /*   By: hdamitzi <hdamitzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 16:28:08 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/03/07 16:32:27 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/03/07 16:43:22 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 size_t	ft_strspn(const char *s, const char *accept)
 {
-		size_t i = 0;
+	size_t	i;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
 		if (ft_strchr(accept, s[i]) == 0)
-			break;
+			break ;
 		++i;
 	}
 	return (i);
